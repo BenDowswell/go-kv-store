28804bbf3704266241229af5f7e27014c95befab
BenDowswell <ben.dowswell2011@gmail.com>
2026-06-27 15:29:58 +0100

    console-splitout: split out console from store functions for later exposure

---
+++ store.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"sync"
+)
+
+type KVStore struct {
+	mu    sync.RWMutex
+	store map[string]string
+}
+
+func NewKVStore() *KVStore {
+	return &KVStore{
+		store: make(map[string]string),
+	}
+}
+
+func (k *KVStore) Get(key string) (string, bool) {
+	k.mu.RLock()
+	defer k.mu.RUnlock()
+	value, ok := k.store[key]
+	return value, ok
+
+}
+
+func (k *KVStore) Set(key, value string) {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+	k.store[key] = value
+}
+
+func (k *KVStore) Delete(key string) {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+	delete(k.store, key)
+}
